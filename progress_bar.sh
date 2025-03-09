0f5c0a41988a599f8c90938e0e26741650386584
rdanielsn <10221010@mahasiswa.itb.ac.id>
2025-03-10 02:18:01 +0700

    feat: static bar size

--- progress_bar.sh
+++ progress_bar.sh
@@ -4,19 +4,28 @@ BAR_CHAR="="
 
 function progress_setup() {
     N_PROCESS="$1"
-    PROCESS_REMAIN="$((N_PROCESS - 1))"
+    BAR_MAX_SIZE=20
+
+    BAR_SIZE=""
     BAR=""
+
+    BLANK_SIZE=""
+    BLANK=""
+
     echo ""
 }
 
 function progress_update() {
     local step="$1"
     local PERCENTAGE
+
     PERCENTAGE=$(bc <<<" 100 * $step / $N_PROCESS")
-    BAR+="$BAR_CHAR"
 
-    BLANK_SPACE=$(printf "%${PROCESS_REMAIN}s")
-    PROCESS_REMAIN=$((PROCESS_REMAIN - 1))
+    BAR_SIZE=$(bc <<<"$PERCENTAGE * $BAR_MAX_SIZE / 100")
+    BAR=$(printf "%${BAR_SIZE}s" | tr ' ' "$BAR_CHAR")
 
-    printf "\r[%s%s] %s%%" "$BAR" "$BLANK_SPACE" "$PERCENTAGE"
+    BLANK_SIZE=$((BAR_MAX_SIZE - BAR_SIZE))
+    BLANK=$(printf "%${BLANK_SIZE}s")
+
+    printf "\r[%s%s] %s%%" "$BAR" "$BLANK" "$PERCENTAGE"
 }
