75f2439c024ee32fd709b841731055a660ff4336
rdanielsn <10221010@mahasiswa.itb.ac.id>
2025-03-10 02:43:16 +0700

    feat: add blank progress bar in the beginning - reorder the simulated process (sleep)

--- dummy_process.sh
+++ dummy_process.sh
@@ -7,8 +7,8 @@ read -rp "Number of process: " n_process
 
 progress_setup "$n_process"
 for i in $(seq 1 "$n_process"); do
+    sleep .75 # simulate a process
     progress_update "$i"
-    sleep .5 # simulate a process
 done
 echo ""
 
--- progress_bar.sh
+++ progress_bar.sh
@@ -6,18 +6,19 @@ function progress_setup() {
     N_PROCESS="$1"
     BAR_MAX_SIZE=20
 
-    BAR_SIZE=""
-    BAR=""
+    PERCENTAGE=0
 
-    BLANK_SIZE=""
-    BLANK=""
+    BAR_SIZE=0
+    BAR=$(printf "%${BAR_SIZE}s" | tr ' ' "$BAR_CHAR")
 
-    echo ""
+    BLANK_SIZE=$((BAR_MAX_SIZE - BAR_SIZE))
+    BLANK=$(printf "%${BLANK_SIZE}s")
+
+    printf "\r[%s%s] %s%%" "$BAR" "$BLANK" "$PERCENTAGE"
 }
 
 function progress_update() {
     local step="$1"
-    local PERCENTAGE
 
     PERCENTAGE=$(bc <<<" 100 * $step / $N_PROCESS")
 
