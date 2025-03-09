bdb7ecf4cbf1f56e67ab2842ad3a6ae6b791694d
rdanielsn <10221010@mahasiswa.itb.ac.id>
2025-03-10 00:58:16 +0700

    feat: add percentage indicator - rename some variables

--- progress_bar.sh
+++ progress_bar.sh
@@ -3,19 +3,20 @@
 BAR_CHAR="="
 
 function progress_setup() {
-    N="$1"
-    N_REMAIN="$(($N - 1))"
-    echo "setup for $N number of process"
+    N_PROCESS="$1"
+    PROCESS_REMAIN="$((N_PROCESS - 1))"
     BAR=""
     echo ""
 }
 
 function progress_update() {
     local step="$1"
+    local PERCENTAGE
+    PERCENTAGE=$(bc <<<" 100 * $step / $N_PROCESS")
     BAR+="$BAR_CHAR"
 
-    BLANK_SPACE=$(printf "%${N_REMAIN}s")
-    N_REMAIN=$(($N_REMAIN - 1))
+    BLANK_SPACE=$(printf "%${PROCESS_REMAIN}s")
+    PROCESS_REMAIN=$((PROCESS_REMAIN - 1))
 
-    printf "\r[%s%s] %s" "$BAR" "$BLANK_SPACE" "$step"
+    printf "\r[%s%s] %s%%" "$BAR" "$BLANK_SPACE" "$PERCENTAGE"
 }
